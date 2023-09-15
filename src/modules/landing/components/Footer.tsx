698c2564b742dbca26fdbf28e71b43ef0de6f749
Ajibade <ajibadeemmanuel58@gmail.com>
2023-09-15 23:25:20 +0100

    landing page rewrite

--- src/modules/landing/components/Footer.tsx
+++ src/modules/landing/components/Footer.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Box,
@@ -8,76 +8,78 @@ import {
   Text,
   useColorModeValue,
   VisuallyHidden,
-} from '@chakra-ui/react'
-import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
-import { ReactNode } from 'react'
-import MoskolLogo from '../../../public/moskolWebLogo.png'
-import Image from 'next/image'
-import Link from 'next/link'
+} from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+import { FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import MoskolLogo from "../../../public/moskolWebLogo.png";
 
 const Logo = (props: any) => {
-  return (
-    <Image alt='logo' src={MoskolLogo}/>
-  )
-}
+  return <Image alt="logo" src={MoskolLogo} />;
+};
 
 const SocialButton = ({
   children,
   label,
   href,
 }: {
-  children: ReactNode
-  label: string
-  href: string
+  children: ReactNode;
+  label: string;
+  href: string;
 }) => {
   return (
     <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
       w={8}
       h={8}
-      cursor={'pointer'}
-      as={'a'}
+      cursor={"pointer"}
+      as={"a"}
       href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
       _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
       <VisuallyHidden>{label}</VisuallyHidden>
       {children}
     </chakra.button>
-  )
-}
+  );
+};
 
 export default function Footer() {
   return (
     <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
+      mt={8}
+      // bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
       <Container
         as={Stack}
-        maxW={'6xl'}
+        maxW={"6xl"}
         py={4}
         spacing={4}
-        justify={'center'}
-        align={'center'}>
-          <Link href={'/'}>
-            <Logo />
-          </Link>
-        <Stack direction={'row'} spacing={6}>
-          <Box as="a" href={'/'}>
+        justify={"center"}
+        align={"center"}
+      >
+        <Link href={"/"}>
+          <Logo />
+        </Link>
+        <Stack direction={"row"} spacing={6}>
+          <Box as="a" href={"/"}>
             Home
           </Box>
-          <Box as="a" href={'/about'}>
+          <Box as="a" href={"/about"}>
             About
           </Box>
-          <Box as="a" href={'/services'}>
+          <Box as="a" href={"/services"}>
             Service
           </Box>
-          <Box as="a" href={'/buy'}>
+          <Box as="a" href={"/buy"}>
             Buy Our Products
           </Box>
         </Stack>
@@ -85,30 +87,32 @@ export default function Footer() {
 
       <Box
         borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+      >
         <Container
           as={Stack}
-          maxW={'6xl'}
+          maxW={"6xl"}
           py={4}
-          direction={{ base: 'column', md: 'row' }}
+          direction={{ base: "column", md: "row" }}
           spacing={4}
-          justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}>
+          justify={{ base: "center", md: "space-between" }}
+          align={{ base: "center", md: "center" }}
+        >
           <Text>© 2023 Moskol Engineering. All rights reserved</Text>
-          <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'Twitter'} href={'#'}>
+          <Stack direction={"row"} spacing={6}>
+            <SocialButton label={"Twitter"} href={"#"}>
               <FaTwitter />
             </SocialButton>
-            <SocialButton label={'YouTube'} href={'#'}>
+            <SocialButton label={"YouTube"} href={"#"}>
               <FaInstagram />
             </SocialButton>
-            <SocialButton label={'Instagram'} href={'#'}>
+            <SocialButton label={"Instagram"} href={"#"}>
               <FaLinkedin />
             </SocialButton>
           </Stack>
         </Container>
       </Box>
     </Box>
-  )
+  );
 }
